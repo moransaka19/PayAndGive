1637cef4acfe9709b2dcd5ba82626a6f238da41e
moransaka19 <47789098+moransaka19@users.noreply.github.com>
2021-06-21 23:49:12 +0300

    success

--- client/src/app/admin/admin.component.ts
+++ client/src/app/admin/admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
+  backups$: Observable<string[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private http: HttpClient) {
   }
 
+  ngOnInit(): void {
+    this.backups$ = this.http.get<string[]>(`${environment.apiUrl}/admin/backups`);
+  }
+
+  onBackup() {
+    this.http.post(`${environment.apiUrl}/admin/backup`, null)
+      .subscribe(x => {
+        this.backups$ = this.http.get<string[]>(`${environment.apiUrl}/admin/backups`);
+      });
+  }
+
+  onRestore(value: string) {
+    this.http.post(`${environment.apiUrl}/admin/restore/${value}`, null)
+      .subscribe();
+  }
 }
